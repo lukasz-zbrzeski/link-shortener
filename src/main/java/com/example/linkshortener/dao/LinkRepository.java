46be2abd423342d949fa83a9273f86c8a0fa8d07
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2020-08-09 18:12:18 +0200

    Add Query method findByShortLink

--- src/main/java/com/example/linkshortener/dao/LinkRepository.java
+++ src/main/java/com/example/linkshortener/dao/LinkRepository.java
@@ -4,6 +4,9 @@ import com.example.linkshortener.dao.entity.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LinkRepository extends JpaRepository<Link, Long> {
+    Optional<Link> findByShortLink(String shortLink);
 }
